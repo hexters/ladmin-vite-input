aa68d28d327e20c04cf1b878163acf759c533a34
Asep <hexters@gmail.com>
2023-01-28 13:30:51 +0700

    fix issue

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
-import modules from "./ladminViteInputs";
-export default ladminViteInputs = (assets) => [...assets, ...modules()];
+import fs from 'fs';
+import path from 'path'
+
+function modules() {
+    let modules = [];
+    let dirModule = path.join('Modules');
+    let dirs = fs.readdirSync(dirModule);
+
+    let types = [{
+        dir: 'js',
+        ext: '.js'
+    },
+    {
+        dir: 'css',
+        ext: '.css'
+    },
+    {
+        dir: 'sass',
+        ext: '.scss'
+    }];
+
+    for (let i = 0; i < dirs.length; i++) {
+
+        for (let x = 0; x < types.length; x++) {
+            modules.push(
+                path.join(dirModule, dirs[i], 'Resources', types[x].dir, dirs[i].toLowerCase() + types[x].ext)
+            );
+        }
+
+    }
+
+    return modules;
+
+}
+
+export default function ladminViteInputs(assets) {
+    return [...assets, ...modules()];
+}
